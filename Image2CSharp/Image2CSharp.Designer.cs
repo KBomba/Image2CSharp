03752cdf25105c4f39afc8a13138dc6da2992ecf
KBomba <King.Bombadil@gmail.com>
2016-02-17 02:43:09 +0100

    Basic implementation

---
+++ Image2CSharp/Image2CSharp.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Image2CSharp
+{
+    partial class Image2CSharp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listFiles = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(136, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open files...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Location = new System.Drawing.Point(154, 0);
+            this.textBox1.MaxLength = 0;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(752, 497);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listFiles
+            // 
+            this.listFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listFiles.FormattingEnabled = true;
+            this.listFiles.HorizontalScrollbar = true;
+            this.listFiles.Location = new System.Drawing.Point(12, 41);
+            this.listFiles.Name = "listFiles";
+            this.listFiles.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listFiles.Size = new System.Drawing.Size(136, 446);
+            this.listFiles.Sorted = true;
+            this.listFiles.TabIndex = 3;
+            // 
+            // Image2CSharp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 497);
+            this.Controls.Add(this.listFiles);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Image2CSharp";
+            this.Text = "Image2CSharp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listFiles;
+    }
+}
+
---
+++ Image2CSharp/Image2CSharp.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image2CSharp
+{
+    public partial class Image2CSharp : Form
+    {
+        private List<FileInfo> _files;
+        private SortedDictionary<string, string> _convertedImages; 
+
+        public Image2CSharp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                Filter = "Image Files(*.PNG;*.BMP;*.JPG;*.GIF)|*.PNG;*.BMP;*.JPG;*.GIF|All files (*.*)|*.*",
+                FilterIndex = 1,
+                Multiselect = true,
+            };
+
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _files = new List<FileInfo>(openFileDialog.FileNames.Length);
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    _files.Add(new FileInfo(file));
+                }
+            }
+
+            PopulateList();
+            ConvertImages();
+            CreateCode();
+        }
+
+        private void PopulateList()
+        {
+            foreach (FileInfo file in _files)
+            {
+                listFiles.Items.Add(file.Name);
+            }
+        }
+
+        private void ConvertImages()
+        {
+            _convertedImages = new SortedDictionary<string, string>();
+            Parallel.ForEach(_files, f =>
+            {
+                string base64 = ImageToBase64(Image.FromFile(f.FullName, true));
+                string imageName = f.Name.Split('.')[0];
+                imageName = imageName[0].ToString().ToUpper()[0] + imageName.Remove(0,1);
+                imageName = imageName.Replace("-","");
+                if (!_convertedImages.ContainsKey(imageName)) _convertedImages.Add(imageName, base64);
+            });
+        }
+
+        public string ImageToBase64(Image image)
+        {
+            string base64String = string.Empty;
+
+            try
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    image.Save(stream, image.RawFormat);
+                    byte[] imageBytes = stream.ToArray();
+
+                    base64String = Convert.ToBase64String(imageBytes);
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+
+            return base64String;
+        }
+        
+
+        private void CreateCode()
+        {
+            StringBuilder stringBuilder = new StringBuilder("using System;");
+            stringBuilder.AppendLine("using System.Drawing;")
+                .AppendLine("using System.IO;")
+                .AppendLine("using System.Text.RegularExpressions;")
+                .AppendLine()
+                .AppendLine("namespace *your namespace*")
+                .AppendLine("{")
+                .AppendLine("class Images".PadLeft(4))
+                .AppendLine("{".PadLeft(4))
+                .AppendLine("private static Image GetIconFromBase64DataUri(string uri)".PadLeft(8))
+                .AppendLine("{".PadLeft(8))
+                .AppendLine("string base64Data = Regex.Match(uri, @\"data: image / (?<type>.+?), (?<data>.+)\")" +
+                            ".Groups[\"data\"].Value;".PadLeft(12))
+                .AppendLine("byte[] binData = Convert.FromBase64String(base64Data);".PadLeft(12))
+                .AppendLine()
+                .AppendLine("Image image;".PadLeft(12))
+                .AppendLine("using (MemoryStream stream = new MemoryStream(binData))".PadLeft(12))
+                .AppendLine("{".PadLeft(12))
+                .AppendLine("image = new Bitmap(stream);".PadLeft(16))
+                .AppendLine("}".PadLeft(12))
+                .AppendLine("return image;".PadLeft(12))
+                .AppendLine("}".PadLeft(8))
+                .AppendLine();
+
+            foreach (string image in _convertedImages.Keys)
+            {
+                stringBuilder.AppendLine(("public static Image " + image + "()").PadLeft(8))
+                    .AppendLine("{".PadLeft(8))
+                    .AppendLine("return GetIconFromBase64DataUri(".PadLeft(12))
+                    .AppendLine(("\"" + _convertedImages[image] + "\"); ").PadLeft(16))
+                    .AppendLine("}".PadLeft(8))
+                    .AppendLine();
+            }
+
+            stringBuilder.AppendLine("}".PadLeft(4)).AppendLine("}");
+
+            textBox1.Text = stringBuilder.ToString();
+        }
+    }
+}
